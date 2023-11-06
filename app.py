a1b92e95107a27ff82ea0ea9f3e67f768cee737c
John Doe <johndoe@email.com>
2023-11-06 09:32:21 +0500

    init

---
+++ app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+async def index():
+    return {
+        "message" : "Welcome to the azure function api"
+    }
